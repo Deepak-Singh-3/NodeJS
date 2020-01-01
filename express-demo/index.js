315f683baed57c6a37c43903848a399a15703dcb
Deepak-Singh-3 <deepaksingh0072@yahoo.com>
2020-01-01 16:12:14 +0530

    Commit

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,9 +1,42 @@
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+//process.env.NODE_ENV //undefined
+//console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+//console.log(`app: ${app.get('env')}`);
+
+app.set('view engine','pug');
+app.set('views','./views'); //default, optional setting
+
+app.use(express.json()); //req.body
 //adding a piece of middleware
+app.use(express.urlencoded({extended : true})); //key=value&key=value
+app.use(express.static('public'));
+app.use(helmet());
+
+//Configuration
+console.log('Application name:' + config.get('name'));
+console.log('Mail server:' + config.get('mail.host'));
+console.log('Mail password:' + config.get('mail.password'));
+
+
+if(app.get('env') === 'development')
+{
+        app.use(morgan('tiny'));
+        console.log('Morgan Enabled');
+}
+app.use(logger);
+
+
+app.use(function(req,res,next) {
+    console.log('Authenticating...');
+    next();
+});
 
 const courses = [
     { id :1, name :'course1'},
@@ -11,7 +44,8 @@ const courses = [
     { id :3, name :'course3'},
 ];
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    //res.send('Hello World');
+    res.render('index', { title : "My Express App", message : "Hello"});
 });
 app.get('/api/courses', (req,res) => {
     res.send(courses);
